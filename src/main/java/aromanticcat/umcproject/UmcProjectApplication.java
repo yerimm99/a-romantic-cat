a4a3c7d2d8c86d6e3837a1e8f403f263836a8c68
Yoon-Jemin <yjm1800@naver.com>
2024-01-09 14:31:41 +0900

    first commit

---
+++ src/main/java/aromanticcat/umcproject/UmcProjectApplication.java
@@ -0,0 +1,13 @@
+package aromanticcat.umcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcProjectApplication.class, args);
+	}
+
+}
