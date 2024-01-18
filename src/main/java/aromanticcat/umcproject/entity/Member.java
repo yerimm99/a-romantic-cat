925a0a27ea12d271ee589ec91a3f93d0a4a3496f
u-genuine <u_genuine@naver.com>
2024-01-19 00:32:36 +0900

    :sparkles: Entity 어노테이션 추가

--- src/main/java/aromanticcat/umcproject/entity/Member.java
+++ src/main/java/aromanticcat/umcproject/entity/Member.java
@@ -1,18 +1,17 @@
 package aromanticcat.umcproject.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import lombok.Getter;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Member extends BaseEntity {
 
     @Id
