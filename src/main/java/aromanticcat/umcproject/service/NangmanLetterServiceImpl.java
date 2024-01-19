34fc724dcecf0c9285674910ead3e993800e5e63
u-genuine <u_genuine@naver.com>
2024-01-19 19:35:09 +0900

    :sparkles: 낭만 우편함 랜덤 닉네임 생성 구현

--- src/main/java/aromanticcat/umcproject/service/NangmanLetterServiceImpl.java
+++ src/main/java/aromanticcat/umcproject/service/NangmanLetterServiceImpl.java
@@ -2,6 +2,7 @@ package aromanticcat.umcproject.service;
 
 import aromanticcat.umcproject.dto.NangmanLetterDTO;
 import aromanticcat.umcproject.entity.NangmanLetter;
+import aromanticcat.umcproject.repository.MemberRepository;
 import aromanticcat.umcproject.repository.NangmanLetterRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -15,14 +16,23 @@ import javax.transaction.Transactional;
 public class NangmanLetterServiceImpl implements NangmanLetterService{
 
     private final ModelMapper modelMapper;
-
     private final NangmanLetterRepository nangmanLetterRepository;
+    private final MemberRepository memberRepository;
+    private final RandomNicknameService randomNicknameService;
 
     @Override
     public Long register(NangmanLetterDTO nangmanLetterDTO){
 
+        //랜덤 닉네임 생성
+        String randomNickname = randomNicknameService.generateRandomNickname();
+
+        //DTO에 랜덤 닉네임 설정
+        nangmanLetterDTO.setSender_nickname(randomNickname);
+
+        //DTO를 엔티티로 변환
         NangmanLetter nangmanLetter = modelMapper.map(nangmanLetterDTO, NangmanLetter.class);
 
+        //엔티티 저장
         Long id = nangmanLetterRepository.save(nangmanLetter).getId();
 
         return id;
--- src/test/java/aromanticcat/umcproject/service/NangmanLetterServiceTests.java
+++ src/test/java/aromanticcat/umcproject/service/NangmanLetterServiceTests.java
@@ -16,18 +16,26 @@ public class NangmanLetterServiceTests {
     @Autowired
     private MemberRepository memberRepository;
 
+    @Autowired
+    private RandomNicknameService randomNicknameService;
+
     @Test
     public void testRegister(){
         System.out.println(nangmanLetterService.getClass().getName());
 
         Member member1 = memberRepository.findById(1L).orElseThrow(() -> new RuntimeException("Member not found"));
 
+        //랜덤 닉네임 생성
+        String randomNickname = randomNicknameService.generateRandomNickname();
+
         NangmanLetterDTO nangmanLetterDTO = NangmanLetterDTO.builder()
-                .content("낭만 편지 샘플2")
+                .content("낭만 편지 샘플3")
                 .is_public(false)
                 .member(member1)
-                .sender_nickname("샘플 닉네임2")
                 .build();
+
+        //생성된 랜덤 닉네임 설정
+        nangmanLetterDTO.setSender_nickname(randomNickname);
 
         Long id = nangmanLetterService.register(nangmanLetterDTO);
 
