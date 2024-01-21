7d90ba373ff19c14dc35704d222f9ecb3533f22e
u-genuine <u_genuine@naver.com>
2024-01-21 15:31:35 +0900

    :sparkles: 구현 안된 기능 주석 처리

--- src/main/java/aromanticcat/umcproject/service/NangmanLetterService.java
+++ src/main/java/aromanticcat/umcproject/service/NangmanLetterService.java
@@ -1,14 +1,13 @@
 package aromanticcat.umcproject.service;
 
 import aromanticcat.umcproject.entity.NangmanLetter;
-import aromanticcat.umcproject.web.dto.NangmanLetterDTO;
 import aromanticcat.umcproject.web.dto.NangmanPostBoxRequestDTO;
 
 public interface NangmanLetterService {
 
     NangmanLetter writeAndSendLetter(NangmanPostBoxRequestDTO.SendLetterDTO requestDTO, String randomNickname);
-
-    NangmanLetterDTO readOne(Long id);
-
-    void receivedReply(NangmanLetterDTO nangmanLetterDTO);
+//
+//    NangmanLetterDTO readOne(Long id);
+//
+//    void receivedReply(NangmanLetterDTO nangmanLetterDTO);
 }
--- src/main/java/aromanticcat/umcproject/service/NangmanLetterServiceImpl.java
+++ src/main/java/aromanticcat/umcproject/service/NangmanLetterServiceImpl.java
@@ -4,21 +4,17 @@ import aromanticcat.umcproject.converter.NangmanPostBoxConverter;
 import aromanticcat.umcproject.entity.NangmanLetter;
 import aromanticcat.umcproject.repository.MemberRepository;
 import aromanticcat.umcproject.repository.NangmanLetterRepository;
-import aromanticcat.umcproject.web.dto.NangmanLetterDTO;
 import aromanticcat.umcproject.web.dto.NangmanPostBoxRequestDTO;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class NangmanLetterServiceImpl implements NangmanLetterService{
 
-    private final ModelMapper modelMapper;
     private final NangmanLetterRepository nangmanLetterRepository;
     private final MemberRepository memberRepository;
     private final RandomNicknameService randomNicknameService;
@@ -30,40 +26,40 @@ public class NangmanLetterServiceImpl implements NangmanLetterService{
 
         return nangmanLetterRepository.save(newNangmanLetter);
     }
-
-    @Override
-    public NangmanLetterDTO readOne(Long id){
-
-        Optional<NangmanLetter> result = nangmanLetterRepository.findById(id);
-
-        NangmanLetter nangmanLetter = result.orElseThrow();
-
-        NangmanLetterDTO nangmanLetterDTO = modelMapper.map(nangmanLetter, NangmanLetterDTO.class);
-
-        return nangmanLetterDTO;
-    }
-
-
-
-    //답장 받을 때 has_response 업데이트
-    @Override
-    public void receivedReply(NangmanLetterDTO nangmanLetterDTO) {
-        Optional<NangmanLetter> result = nangmanLetterRepository.findById(nangmanLetterDTO.getId());
-
-        NangmanLetter nangmanLetter = result.orElseThrow();
-
-        //has_response 값 업데이트
-        nangmanLetter.change(nangmanLetterDTO.getHas_response());
-
-        //is_public 값에 따라 낭만 모음집에 추가
-        if(nangmanLetter.getIs_public()){
-            addLetterToNangmanCollectoin(nangmanLetter);
-        }
-        nangmanLetterRepository.save(nangmanLetter);
-    }
-
-    private void addLetterToNangmanCollectoin(NangmanLetter nangmanLetter){
-        // 낭만 모음집에 해당 편지를 추가하는 로직을 구현
-        // 추가되는 방법은 해당 모음집의 컬렉션에 편지를 추가하거나 다른 적절한 방법으로 구현
-    }
+//
+//    @Override
+//    public NangmanLetterDTO readOne(Long id){
+//
+//        Optional<NangmanLetter> result = nangmanLetterRepository.findById(id);
+//
+//        NangmanLetter nangmanLetter = result.orElseThrow();
+//
+//        NangmanLetterDTO nangmanLetterDTO = modelMapper.map(nangmanLetter, NangmanLetterDTO.class);
+//
+//        return nangmanLetterDTO;
+//    }
+//
+//
+//
+//    //답장 받을 때 has_response 업데이트
+//    @Override
+//    public void receivedReply(NangmanLetterDTO nangmanLetterDTO) {
+//        Optional<NangmanLetter> result = nangmanLetterRepository.findById(nangmanLetterDTO.getId());
+//
+//        NangmanLetter nangmanLetter = result.orElseThrow();
+//
+//        //has_response 값 업데이트
+//        nangmanLetter.change(nangmanLetterDTO.getHas_response());
+//
+//        //is_public 값에 따라 낭만 모음집에 추가
+//        if(nangmanLetter.getIs_public()){
+//            addLetterToNangmanCollectoin(nangmanLetter);
+//        }
+//        nangmanLetterRepository.save(nangmanLetter);
+//    }
+//
+//    private void addLetterToNangmanCollectoin(NangmanLetter nangmanLetter){
+//        // 낭만 모음집에 해당 편지를 추가하는 로직을 구현
+//        // 추가되는 방법은 해당 모음집의 컬렉션에 편지를 추가하거나 다른 적절한 방법으로 구현
+//    }
 }
