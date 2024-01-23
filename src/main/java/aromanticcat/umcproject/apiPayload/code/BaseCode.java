98f961a1220f67535b20365cdd2e36913136a52a
yerim <yerimm99@github.com>
2024-01-24 03:02:20 +0900

    🔧git cache 삭제, gitignore 등록

--- src/main/java/aromanticcat/umcproject/apiPayload/code/BaseCode.java
+++
@@ -1,7 +0,0 @@
-package aromanticcat.umcproject.apiPayload.code;
-
-public interface BaseCode {
-    public ReasonDTO getReason();
-
-    public ReasonDTO getReasonHttpStatus();
-}
--- src/main/java/aromanticcat/umcproject/service/FriendService/FriendCommandServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package aromanticcat.umcproject.service.FriendService;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class FriendCommandServiceImpl implements FriendCommandService {
-}
