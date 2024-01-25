ae1ecba9101522fdaa3bc5a7688b6cc6c8de4da4
yerim <86309538+yerimm99@users.noreply.github.com>
2024-01-26 00:01:03 +0900

    ✨Oauth2 소셜 로그인 API 연동

---
+++ src/main/java/aromanticcat/umcproject/security/oauth/MyAuthenticationSuccessHandler.java
@@ -0,0 +1,87 @@
+package aromanticcat.umcproject.security.oauth;
+
+import aromanticcat.umcproject.security.jwt.GeneratedToken;
+import aromanticcat.umcproject.security.jwt.JwtUtil;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+
+        // OAuth2User로 캐스팅하여 인증된 사용자 정보를 가져온다.
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        // 사용자 이메일을 가져온다.
+        String email = oAuth2User.getAttribute("email");
+        // 서비스 제공 플랫폼(GOOGLE, KAKAO, NAVER)이 어디인지 가져온다.
+        String provider = oAuth2User.getAttribute("provider");
+
+        // CustomOAuth2UserService에서 셋팅한 로그인한 회원 존재 여부를 가져온다.
+        boolean isExist = oAuth2User.getAttribute("exist");
+        // OAuth2User로 부터 Role을 얻어온다.
+        String role = oAuth2User.getAuthorities().stream().
+                findFirst() // 첫번째 Role을 찾아온다.
+                .orElseThrow(IllegalAccessError::new) // 존재하지 않을 시 예외를 던진다.
+                .getAuthority(); // Role을 가져온다.
+
+        // 회원이 존재할경우
+        if (isExist) {
+            // 회원이 존재하면 jwt token 발행을 시작한다.
+            GeneratedToken token = jwtUtil.generateToken(email, role);
+            log.info("jwtToken = {}", token.getAccessToken());
+
+            // accessToken을 쿼리스트링에 담는 url을 만들어준다.
+            String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/loginSuccess")
+                    //.queryParam("accessToken", token.getAccessToken())
+                    .build()
+                    .encode(StandardCharsets.UTF_8)
+                    .toUriString();
+            // JWT 토큰을 Response Header에 설정
+            response.addHeader("Authorization", token.getAccessToken());
+            log.info("redirect 준비");
+
+            SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
+
+            if (savedRequest != null) {
+                targetUrl = savedRequest.getRedirectUrl();
+            }
+
+            // 사용자가 이전에 있던 페이지로 리다이렉트 시킨다.
+            getRedirectStrategy().sendRedirect(request, response, targetUrl);
+
+
+        } else {
+
+            // 회원이 존재하지 않을경우, 서비스 제공자와 email을 쿼리스트링으로 전달하는 url을 만들어준다.
+            String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/signUp")
+                    .queryParam("email", (String) oAuth2User.getAttribute("email"))
+                    .queryParam("provider", provider)
+                    .build()
+                    .encode(StandardCharsets.UTF_8)
+                    .toUriString();
+            log.info("redirect 준비");
+            // 회원가입 페이지로 리다이렉트 시킨다.
+            getRedirectStrategy().sendRedirect(request, response, targetUrl);
+        }
+    }
+
+}
