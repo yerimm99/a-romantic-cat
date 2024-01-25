cb65d7d11e302dc1b703ba42b290dd2dcb40d768
yerim <86309538+yerimm99@users.noreply.github.com>
2024-01-26 00:02:43 +0900

    ✨jwt token 발급 및 인증

---
+++ src/main/java/aromanticcat/umcproject/security/jwt/JwtExceptionFilter.java
@@ -0,0 +1,34 @@
+package aromanticcat.umcproject.security.jwt;
+
+import aromanticcat.umcproject.apiPayload.code.status.ErrorStatus;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.JwtException;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (JwtException e) {
+            response.setStatus(401);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+            objectMapper.writeValue(response.getWriter(), ErrorStatus.MEMBER_NOT_FOUND);
+        }
+    }
+}
---
+++ src/main/java/aromanticcat/umcproject/web/controller/AuthController.java
@@ -0,0 +1,73 @@
+package aromanticcat.umcproject.web.controller;
+
+import aromanticcat.umcproject.apiPayload.ApiResponse;
+import aromanticcat.umcproject.repository.RefreshTokenRepository;
+import aromanticcat.umcproject.security.StatusResponseDto;
+import aromanticcat.umcproject.security.jwt.JwtUtil;
+import aromanticcat.umcproject.security.jwt.RefreshToken;
+import aromanticcat.umcproject.security.jwt.RefreshTokenService;
+import java.util.Optional;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final RefreshTokenRepository tokenRepository;
+    private final RefreshTokenService tokenService;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("token/logout")
+    public ApiResponse<StatusResponseDto> logout(@RequestHeader("Authorization") final String accessToken) {
+
+        // 엑세스 토큰으로 현재 Redis 정보 삭제
+        tokenService.removeRefreshToken(accessToken);
+        return ApiResponse.onSuccess(StatusResponseDto.addStatus(200));
+    }
+
+    @PostMapping("/token/refresh")
+    public ResponseEntity<TokenResponseStatus> refresh(@RequestHeader("Authorization") final String accessToken) {
+
+        // 액세스 토큰으로 Refresh 토큰 객체를 조회
+        Optional<RefreshToken> refreshToken = tokenRepository.findByAccessToken(accessToken);
+
+        // RefreshToken이 존재하고 유효하다면 실행
+        if (refreshToken.isPresent() && jwtUtil.verifyToken(refreshToken.get().getRefreshToken())) {
+            // RefreshToken 객체를 꺼내온다.
+            RefreshToken resultToken = refreshToken.get();
+            // 권한과 아이디를 추출해 새로운 액세스토큰을 만든다.
+            String newAccessToken = jwtUtil.generateAccessToken(resultToken.getId(),
+                    jwtUtil.getRole(resultToken.getRefreshToken()));
+            // 액세스 토큰의 값을 수정해준다.
+            resultToken.updateAccessToken(newAccessToken);
+            tokenRepository.save(resultToken);
+            // 새로운 액세스 토큰을 반환해준다.
+            return ResponseEntity.ok(TokenResponseStatus.addStatus(200, newAccessToken));
+        }
+
+        return ResponseEntity.badRequest().body(TokenResponseStatus.addStatus(400, null));
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class TokenResponseStatus<T> {
+
+        private Integer code;
+        private String msg;
+
+        public static TokenResponseStatus addStatus(Integer code, String msg) {
+            return new TokenResponseStatus(code, msg);
+        }
+
+
+    }
+
+}
