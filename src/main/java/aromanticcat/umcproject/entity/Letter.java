f87f4b1fa3ae6bd52f59f5bbc02ea7bf79e8d907
Eunkyy <melon0802@naver.com>
2024-01-30 05:17:54 +0900

    :sparkles: Feat : 엔티티, dto 추가

--- src/main/java/aromanticcat/umcproject/entity/Letter.java
+++ src/main/java/aromanticcat/umcproject/entity/Letter.java
@@ -1,6 +1,7 @@
 package aromanticcat.umcproject.entity;
 
-import lombok.Builder;
+import aromanticcat.umcproject.web.dto.Letterbox.LetterResponse;
+import aromanticcat.umcproject.web.dto.Letterbox.LetterboxResponse;
 import lombok.Getter;
 import lombok.*;
 import javax.persistence.*;
@@ -16,7 +17,7 @@ public class Letter extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long letter_id;
+    private Long letterId;
 
     @Column(name = "nickname", length = 60)
     private String nickname;
@@ -33,14 +34,38 @@ public class Letter extends BaseEntity {
     private Long senderId;
 
     @ManyToOne
-    @JoinColumn(name = "letter_paper_id",insertable = false, updatable = false)
+    @JoinColumn(name = "letter_paper_id")
     private LetterPaper letterPaper;
 
     @ManyToOne
-    @JoinColumn(name = "stamp_id", insertable = false, updatable = false)
+    @JoinColumn(name = "stamp_id")
     private Stamp stamp;
 
     @ManyToOne
-    @JoinColumn(name = "id", insertable = false, updatable = false)
+    @JoinColumn(name = "id")
     private Letterbox letterbox;
+
+
+    @Builder
+    public Letter(String nickname, String content, boolean open, Boolean loginStatus, Long senderId) {
+        this.nickname = nickname;
+        this.content = content;
+        this.open = open;
+        this.loginStatus = loginStatus;
+        this.senderId = senderId;
+    }
+
+    public LetterResponse toResponse(Letter letter) {
+        LetterResponse letterResponse = new LetterResponse();
+        letterResponse.setNickname(letter.nickname);
+        letterResponse.setContent(letter.content);
+        letterResponse.setOpen(letter.open);
+        letterResponse.setLoginStatus(letter.loginStatus);
+        letterResponse.setSenderId(letter.senderId);
+        letterResponse.setLetterboxId(letter.letterbox.getLetterbox_id());
+        letterResponse.setLetterPaperId(letter.letterPaper.getId());
+        letterResponse.setStampId(letter.stamp.getId());
+
+        return letterResponse;
+    }
 }
--- src/main/java/aromanticcat/umcproject/web/dto/Letterbox/LetterboxResponse.java
+++ src/main/java/aromanticcat/umcproject/web/dto/Letterbox/LetterboxResponse.java
@@ -1,4 +1,4 @@
-package aromanticcat.umcproject.web.dto;
+package aromanticcat.umcproject.web.dto.Letterbox;
 
 import lombok.Data;
 
