2fbf5b63e1a1fb4b8165edb0ba09e0836df45276
Eunkyy <melon0802@naver.com>
2024-01-30 02:13:31 +0900

    :sparkles: Feat: Letterbox 생성

---
+++ src/main/java/aromanticcat/umcproject/service/letterbox/LetterboxService.java
@@ -0,0 +1,30 @@
+package aromanticcat.umcproject.service.letterbox;
+
+import aromanticcat.umcproject.entity.Letterbox;
+import aromanticcat.umcproject.entity.Member;
+import aromanticcat.umcproject.repository.LetterBoxRepository;
+import aromanticcat.umcproject.repository.MemberRepository;
+import aromanticcat.umcproject.web.dto.LetterboxRequest;
+import aromanticcat.umcproject.web.dto.LetterboxResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class LetterboxService {
+    private final LetterBoxRepository letterboxRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public String createLetterbox(LetterboxRequest letterboxRequest) {
+        Member member = memberRepository.findById(letterboxRequest.getMember_id())
+                .orElseThrow(() -> new IllegalArgumentException("Member not found. id=" + letterboxRequest.getMember_id()));
+
+        Letterbox letterbox = letterboxRequest.toEntity();
+        letterbox.setMember(member);
+        letterboxRepository.save(letterbox);
+        return letterbox.getName();
+    }
+}
---
+++ src/main/java/aromanticcat/umcproject/web/dto/LetterboxResponse.java
@@ -0,0 +1,11 @@
+package aromanticcat.umcproject.web.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class LetterboxResponse {
+    private Long id;
+    private String name;
+}
