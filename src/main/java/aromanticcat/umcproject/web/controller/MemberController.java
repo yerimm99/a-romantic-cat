e92bbd955924e3999f633f3246d18f7cd3296aed
yerim <86309538+yerimm99@users.noreply.github.com>
2024-01-30 13:33:02 +0900

    ✨예외 처리

--- src/main/java/aromanticcat/umcproject/web/controller/MemberController.java
+++ src/main/java/aromanticcat/umcproject/web/controller/MemberController.java
@@ -5,7 +5,9 @@ import aromanticcat.umcproject.converter.MemberConverter;
 import aromanticcat.umcproject.entity.Member;
 import aromanticcat.umcproject.service.MemberService;
 import aromanticcat.umcproject.web.dto.MemberResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,11 +19,15 @@ public class MemberController {
     private MemberService service;
 
     @PostMapping("/signUp")
+    @Operation(summary = "회원가입 API", description = "회원가입 API입니다.")
     public ApiResponse<MemberResponseDTO.MemberJoinResultDTO> signup(@RequestBody String nickname,
                                                                      @RequestParam String email) {
+        try {
+            Member user = service.createUser(email, nickname);
 
-        Member user = service.createUser(email, nickname);
-
-        return ApiResponse.onSuccess(MemberConverter.toMemberDTO(user));
+            return ApiResponse.onSuccess(MemberConverter.toMemberDTO(user));
+        } catch (Exception e) {
+            return ApiResponse.onFailure(HttpStatus.INTERNAL_SERVER_ERROR.toString(), e.getMessage(), null);
+        }
     }
 }
