2f00eee1a68a6a4bc062a10baa1f4f1944053336
Eunkyy <melon0802@naver.com>
2024-02-04 05:54:14 +0900

    :sparkles: add myDesign entity, dto

---
+++ src/main/java/aromanticcat/umcproject/entity/MyLetterPaper.java
@@ -0,0 +1,36 @@
+package aromanticcat.umcproject.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Table(name = "myLetterPaper")
+public class MyLetterPaper {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mPaper_id;
+
+    @NotNull
+    private String imageUrl;
+
+    @NotNull
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    public MyLetterPaper(Long id, String imageUrl, String name, Member member) {
+        this.mPaper_id = id;
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.member = member;
+    }
+
+}
---
+++ src/main/java/aromanticcat/umcproject/entity/MyStamp.java
@@ -0,0 +1,39 @@
+package aromanticcat.umcproject.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@RequiredArgsConstructor
+@Table(name = "myStamp")
+public class MyStamp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mStamp_id;
+
+    @NotNull
+    private String imageUrl;
+
+    @NotNull
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    public MyStamp(Long id, String imageUrl, String name, Member member) {
+        this.mStamp_id = id;
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.member = member;
+    }
+}
