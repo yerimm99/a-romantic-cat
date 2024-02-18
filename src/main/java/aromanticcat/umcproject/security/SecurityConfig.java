15de62a6bfa0159f0a567f446e402cca5739d18a
Yoon-Jemin <yjm1800@naver.com>
2024-02-18 18:56:51 +0900

    feat: 주소록 정렬 추가

--- src/main/java/aromanticcat/umcproject/security/SecurityConfig.java
+++ src/main/java/aromanticcat/umcproject/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                 .antMatchers("/", "/login", "/signUp", "/nangman-collection/",
                         "/nangman-collection/{nangmanLetterId}/like",
                         "/nangman-collection/{nangmanLetterId}",
-                        "/address-book/,", "/address-book/close-friends",
+                        "/address-book/", "/address-book/close-friends",
                         "/store/letter-papers").permitAll()
                 .antMatchers("/token/**").permitAll() // 토큰 발급을 위한 경로는 모두 허용
                 .antMatchers("/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**").permitAll()
