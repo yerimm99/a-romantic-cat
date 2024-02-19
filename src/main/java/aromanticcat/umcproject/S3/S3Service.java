979cdbf02d1b4deb00a137a2514591b9899ba64c
u-genuine <101379241+u-genuine@users.noreply.github.com>
2024-02-19 22:11:28 +0900

    :bug: s3 url 변경 (#175)

--- src/main/java/aromanticcat/umcproject/S3/S3Service.java
+++ src/main/java/aromanticcat/umcproject/S3/S3Service.java
@@ -2,14 +2,15 @@ package aromanticcat.umcproject.S3;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import java.io.IOException;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -22,7 +23,7 @@ public class S3Service {
     private String bucketName;
     private String dir1 = "/letter-paper";
     private String dir2 = "/stamp";
-    private String defaultUrl = "https://airplanning-bucket.s3.ap-northeast-2.amazonaws.com";
+    private String defaultUrl = "https://romantic-cat-bucket.s3.ap-northeast-2.amazonaws.com";
 
     public String uploadFile1(MultipartFile file) throws IOException {
 
