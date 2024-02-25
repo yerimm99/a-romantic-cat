ec6d8ce8e013a3fafd151e82381a1c865f2f3d05
u-genuine <101379241+u-genuine@users.noreply.github.com>
2024-02-26 00:43:13 +0900

    ✨ refreshToken API 추가 (#197)
    * :sparkles: Security, Swagger 설정 수정, 일반 로그인 구현 완료
    
    * :bug: cors 활성화
    
    * :sparkles: refreshToken api 추가

--- src/main/java/aromanticcat/umcproject/web/controller/MemberApiController.java
+++ src/main/java/aromanticcat/umcproject/web/controller/MemberApiController.java
@@ -5,11 +5,9 @@ import aromanticcat.umcproject.web.dto.Member.MemberSignInRequestDto;
 import aromanticcat.umcproject.web.dto.Member.MemberSignupRequestDto;
 import aromanticcat.umcproject.web.dto.Member.TokenResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RestController
@@ -26,5 +24,10 @@ public class MemberApiController {
     @PostMapping("/login")
     public TokenResponseDto login(@RequestBody @Valid MemberSignInRequestDto requestDto){
         return memberService.login(requestDto);
+    }
+
+    @PutMapping("/refreshToken")
+    public TokenResponseDto issueAccessToken(HttpServletRequest request){
+        return memberService.issueAccessToken(request);
     }
 }
